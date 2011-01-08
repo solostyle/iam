21e09ea975d839620d502589a3a01c5f658f3ea2
solostyle <1style@gmail.com>
2011-01-08 15:55:54 -0600

    Created a bash script that creates new controller/model classes and views for ease of use

---
+++ scripts/newcontroller.sh
@@ -0,0 +1,26 @@
+if [ "$1" = "" ]
+then
+	echo "You must enter a name in singular form"
+	exit 1
+else
+	MODEL=$1
+fi
+
+if [ "$2" = "" ]
+then
+	echo "You must enter a name in plural form"
+	exit 1
+else
+	CONTROLLER=$2
+fi
+
+cd /mnt/home/a/www/iam/app/
+cp controllers/shellscontroller.php controllers/"$CONTROLLER"controller.php
+gvim controllers/"$CONTROLLER"controller.php
+
+cp models/shell.php models/"$MODEL".php
+gvim models/"$MODEL".php
+
+cp views/shells/ views/"$CONTROLLER"/ -r
+gvim views/"$CONTROLLER"/index.php
+
