a956e307bd5cf6dc6a414d58975ef81ba366aa9d
solostyle <1style@gmail.com>
2011-01-16 18:46:41 -0600

    Removed code from views/shells/index.php that loaded latest three blog entries b/c now we load them through ajax

--- app/views/shells/index.php
+++ app/views/shells/index.php
@@ -1,9 +1,5 @@
     <div id="blogEntries">
-        <?php 
-            select_db();
-            print implode('', array_map("make_entry", rtrv_entries('', 3)));
-            mysql_close();
-        ?>
+
     </div><!-- end #blogEntries -->
 </div><!-- end #right -->
 
