d2d99944a195e6cce28d41e9496fa4671ffd933b
solostyle <1style@gmail.com>
2011-01-17 11:30:26 -0600

    Blog controller now uses setLimit() to set the limit for the index() function

--- app/controllers/blogcontroller.php
+++ app/controllers/blogcontroller.php
@@ -11,10 +11,10 @@ class BlogController extends Controller {
 
   function index() {
     $this->doNotRenderHeader = true;
-    //$this->Entry->regexp('id','^2010'); // need a better way to enter a limit
-    //$this->Entry->orderBy('time','DESC');
-    //$this->set('blog', $this->Entry->search());
-    $this->set('blog', $this->Entry->custom("SELECT * FROM `blog` ORDER BY `time` DESC LIMIT 3"));
+    $this->Entry->setLimit(3);
+    $this->Entry->orderBy('time','DESC');
+    $this->set('blog', $this->Entry->search());
+    //$this->set('blog', $this->Entry->custom("SELECT * FROM `blog` ORDER BY `time` DESC LIMIT 3"));
   }
 
     // not using yet
