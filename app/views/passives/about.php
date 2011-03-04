f794a73bef65e8647c5c6f44db6fa8de5b81508f
solostyle <1style@gmail.com>
2011-03-04 12:00:14 +0530

    Working on modifying the about page

--- app/views/passives/about.php
+++ app/views/passives/about.php
@@ -1,4 +1,8 @@
-    <div>
-Coming forthwith
-    </div><!-- end about div -->
+    <div class="main">
+This blog explores ideas and chronicles my experiences as a student of the internal arts. I've been studying meditation for a number of years, becoming more involved in recent years. The Chinese Qi Gong tradition was my first introduction to the meditation with which I most resonate today. I have also discovered the principles of Patanjali's Ashtanga Yoga and incorporate them into my Hatha Yoga practice. I have created an integrated practice for myself that addresses my day-to-day imbalances. I try to talk about ideas and experiences here that I think are worth sharing with others, and I hope that it serves some purpose in your life. Namaskara.
+    </div><!-- end about's main div -->
+    <div class="info">
+<img src="" />
+    </div><!-- end about's main div -->
+
 </div><!-- end #right -->
