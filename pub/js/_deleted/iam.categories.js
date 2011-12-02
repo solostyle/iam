6eed145a8c42f383b43c033b0c3608be84694bb0
solostyle <spam@solostyle.net>
2011-12-02 00:24:50 -0600

    removed ids, tags, and categories controllers. simplified

---
+++ pub/js/_deleted/iam.categories.js
@@ -0,0 +1,42 @@
+this.Iam.Categories = this.Iam.Categories || function() {
+
+    // Globals, bah!
+    var root = "http://iam.solostyle.net", ds = "/";
+
+    // Elements
+    var blogWPElem = function() {return Ydom.get('blogEntries');};
+
+    // Success and failure functions for different requests
+    var handleFailure = function(o){
+        if(o.responseText !== undefined){
+            blogWPElem().innerHTML = "request failure: " + o.responseText + blogWPElem().innerHTML;
+        }
+    };
+
+    var handleSuccess = function(o) {
+        if(o.responseText !== undefined){
+            blogWPElem().innerHTML = o.responseText;
+        }
+    };
+
+    var callback ={
+        method:"GET",
+        success: handleSuccess,
+        failure: handleFailure
+    };
+
+    var catRequest = function(cat){
+        cat = cat.replace(/[_]/gi, " ");
+        var requestStr = root+ds+'blog/category/1/'+cat;
+        var request = AjaxR(requestStr, callback);
+    };
+
+    return {
+
+        Load: function(cat){
+            // initial load
+            catRequest(cat);
+        }
+    };
+
+}();
